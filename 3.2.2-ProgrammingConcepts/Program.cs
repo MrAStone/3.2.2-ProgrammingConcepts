16cadbcaa1749b7f9052b50f5818957ae831c137
A Stone <astone@heckgrammar.co.uk>
2023-04-27 14:17:18 +0100

    Add project files.

---
+++ 3.2.2-ProgrammingConcepts/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2._2_ProgrammingConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  variable declaration
+            // dataType Idendtifier (value) ;
+            int num;
+            string myString = "This is a string";
+            //• constant declaration
+            // const dataType Identifier = value ;
+            const double planck = 6.62607015e-34;
+            //• assignment
+            // variable = value ;
+            num = 123;
+            myString = "This is an assignment";
+            // assignemnt from user input
+            // Console.ReadLine();
+            // Always reads as a string
+            // need to convert to any other data type
+            // dataType.Parse(Console.Readline());
+            Console.Write("Enter some text: "); //output same line ending
+            myString = Console.ReadLine();
+            Console.Write("Enter a value for a new string: ");
+            string newString = Console.ReadLine();
+            // Non string inputs
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write("Enter a single character: ");
+            char c = char.Parse(Console.ReadLine());
+            Console.Write("Enter a floating point number: ");
+            double d = double.Parse(Console.ReadLine());
+            //• iteration
+            // for loops
+            //
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            //User input to guide number of times to loop
+            Console.Write("Enter how many times to loop: ");
+            num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
+            //Loop with diferent increment
+            for (int i = 0; i <= 100; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+            //Loop down
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            // Condition controlled looping
+            num = 10;
+            while (num > 0)
+            {
+                num--;
+            }
+            num = 10;
+            do
+            {
+                num -= 2;
+            }while (num > 0);
+            // Nested iteration
+            bool found = false;
+            while (!found)
+            {
+
+            }
+            //• selection
+            // if(criteria){ thing to do }
+            // Compari
+            
+            //• subroutine(procedure / function)
+        }
+    }
+}
